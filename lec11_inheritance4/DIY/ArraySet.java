c1074a134a6f10a9b9df81fcac51d28b0f0b1864
Josh Hug <hug@cs.berkeley.edu>
2022-09-19 13:36:23 -0700

    add starter code for students who want to do it themselves

---
+++ lec11_inheritance4/DIY/ArraySet.java
@@ -0,0 +1,10 @@
+package lec11_inheritance4.DIY;
+
+public class ArraySet<T> {
+    /* To do:
+    0. Implement ArraySet operations: contains, add, size
+    1. Make ArraySet implement the Iterable<T> interface.
+    2. Implement a toString method.
+    3. Implement an equals() method.
+    */
+}
